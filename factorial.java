9e1bce5bc17dfb7b491f079415b4174d3172a48b
Sai-Durga-purella <71429521+Sai-Durga-purella@users.noreply.github.com>
2021-07-16 13:35:50 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,34 @@
+class sample
+
+{
+ static int factorial(int n)
+
+ {
+ if (n == 0)
+ return 1;   
+ else    
+    
+return(n * factorial(n-1));    
+
+ }  
+ public static void main(String args[])
+
+ {  
+
+ int i,fact=1;  
+
+ int number=7; 
+ 
+fact = factorial(number);
+ System.out.println("Factorial of "+number+"  "+fact);    
+
+ } 
+ 
+} 
+
+
+...........................................
+
+ 
+Output:
+Factorial of 7  5040
